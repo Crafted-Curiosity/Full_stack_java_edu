20bcb6ed901807625cecee720179f7d8c4e0171e
Soumyajit Sinha <soumyajitsinha088@gmail.com>
2025-05-18 20:13:38 +0530

    footer ready

---
+++ src/components/Footer.jsx
@@ -0,0 +1,90 @@
+import React from "react";
+import logo from "../assets/crafted-curiosity.png";
+import '../styling/Footer.css'
+import { FaGithub, FaLinkedin, FaWhatsapp, FaYoutube , FaDiscord} from "react-icons/fa";
+import { MdEmail, MdPhone } from "react-icons/md";
+
+
+
+const Footer = () => {
+  return (
+    <section className="footer">
+      <footer className="footer-bar">
+        <div className="footer-logo span-div">
+          <img src={logo} alt="Crafted Curiosity Logo" />
+        </div>
+
+        <div className="vertical-line"></div>
+
+        <div className="footer-links span-div">
+          <ul>
+            <li>
+              {/* Replace with icon later */}
+              <span><FaYoutube /> </span>
+              <a href="https://youtube.com/@craftedcuriosity" target="_blank" rel="noopener noreferrer">
+                Crafted Curiosity
+              </a>
+            </li>
+            <li>
+              <span><FaGithub /> </span>
+              <a href="https://github.com/Crafted-Curiosity" target="_blank" rel="noopener noreferrer">
+                Crafted Curiosity
+              </a>
+            </li>
+            <li>
+              <span><FaGithub />  </span>
+              <a href="https://github.com/Soumyajit-siliguri" target="_blank" rel="noopener noreferrer">
+                Soumyajit Sinha
+              </a>
+            </li>
+            <li>
+              <span><FaLinkedin /> </span>
+              <a href="https://linkedin.com/in/soumyajit-sinha" target="_blank" rel="noopener noreferrer">
+                Soumyajit Sinha
+              </a>
+            </li>
+            <li>
+              <span><FaWhatsapp /> </span>
+              <a href="https://wa.me/919594040621" target="_blank" rel="noopener noreferrer">
+                Chat with Soumyajit
+              </a>
+            </li>
+            <li>
+              <span><FaDiscord /> </span>
+              <a href="https://discordapp.com/users/731234687846187059" target="_blank" rel="noopener noreferrer">
+                Discord
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="vertical-line"></div>
+
+        <div className="my-address span-div">
+          <address>
+            <p><strong>Crafted Curiosity</strong></p>
+            <p>SRA B2C, Andheri East, MIDC</p>
+            <p>Mumbai, Maharashtra, 400 093</p>
+            <p><MdEmail /> <a href="mailto:soumyajitsinha18@outlook.com">soumyajitsinha18@outlook.com</a></p>
+            <p><MdPhone /> <a href="tel:+919594040621">+91 95940 40621</a></p>
+          </address>
+        </div>
+
+        
+      </footer>
+      <div className="footer-copyright">
+          <hr />
+          <p>
+            © 2025 Crafted Curiosity. All rights reserved.<br />
+            Site developed by{" "}
+            <a href="https://github.com/Soumyajit-siliguri" target="_blank" rel="noopener noreferrer">
+              Soumyajit Sinha
+            </a>.
+          </p>
+          <hr />
+      </div>
+    </section>
+  );
+};
+
+export default Footer;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "../components/Header";
-// import Footer from "../components/Footer";
+import Footer from "../components/Footer";
 
 const HomePage = () => {
     /*useEffect(() => {
@@ -11,6 +11,7 @@ const HomePage = () => {
   return (
     <>
       <Header />
+      <Footer />
     </>
   );
 };
