f0898b6e2d41df270a8d5ab72cd9f337206d8912
Soumyajit Sinha <soumyajitsinha088@gmail.com>
2025-05-18 18:10:04 +0530

    header ready

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -22,8 +22,8 @@ const Header = () => {
                     <ul>
                         <li><a href="/aboutus">About Us</a></li>
                         <li><a href="/projects">Projects</a></li>
-                        <li><a href="#">Contact</a></li>
-                        <li><a href="#">CC Network</a></li>
+                        <li><a href="/">Contact</a></li>
+                        <li><a href="/">CC Network</a></li>
                     </ul>
                 </nav>
             </header>
