aa04e9db1a0e3ccb26453b8a319087a6849093ef
Soumyajit Sinha <soumyajitsinha088@gmail.com>
2025-05-24 00:33:58 +0530

    styling done on homepage -1.

--- src/components/homepagecomponents/Welcomer.jsx
+++ src/components/homepagecomponents/Welcomer.jsx
@@ -7,7 +7,7 @@ const Welcomer = () => {
   return (
     <section className="wolcomer">
         <div className="welcomer-header">
-            <img src={bgimage} alt="Crafted Curiosity back ground image" className="welcomer-bgimage" />
+            <img src={bgimage} alt="Crafted Curiosity wallpaper" className="welcomer-bgimage" />
             <div className="welcomer-text">
                 <h1>Welcome to Crafted Curiosity - Edu</h1>
                 <h4>Engage | Create | Transform</h4>
